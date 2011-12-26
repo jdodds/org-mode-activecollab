533ab1711efdbf7fef60c000ff496da7f387f117
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2011-12-26 01:55:37 -0500

    found another edge case leading to hour-long rewrites

--- org-feed-active-collab.el
+++ org-feed-active-collab.el
@@ -1,48 +1,38 @@
 (defun org-feed-parse-activecollab-entry (entry)
   "Parses an ActiveCollab RSS entry and returns an Org outline node with useful
-  information."
+  information." 
   (require 'xml)
   (let ((desc (plist-get entry :description))
-	(link (plist-get entry :guid)))
-    (let ((due-date (not (null (string-match "Due on:" desc)))))
-      (if due-date
-	  (progn
-	    (string-match
-	     "Ticket:</b> <[^>]+>\\([^<]+\\).+<b>Due on:</b>\\([^<]+\\).+<hr /> <p>\\([^<]+\\)<"
-	     desc)
-	    (let ((headline (match-string 1 desc))
-		  (deadline (match-string 2 desc))
-		  (description (match-string 3 desc)))
-
-	      (concat
-	       "* TODO [["
-	       link
-	       "]["
-	       headline
-	       "]]"
-	       "\nDEADLINE: <"
-	       (format-time-string
-		"%Y-%m-%d %a"
-		(apply 'encode-time
-		       (org-fix-decoded-time (parse-time-string deadline))))
-	       ">\n"
-	       description)))
+	(link (plist-get entry :guid))
+	(deadline nil)
+	(headline nil)
+	(description nil))
+    (if (string-match-p "Due on:" desc)
 	(progn
-	  (string-match
-	   "Ticket:</b> <[^>]+>\\([^<]+\\).+<hr />\\([^]]+\\)"
-	   desc)
-	  (let ((headline (match-string 1 desc))
-		(description (match-string 2 desc)))
-	    (concat
-	     "* TODO [["
-	     link
-	     "]["
-	     headline
-	     "]]"
-	     "\n"
-	     description)))))))
-      
-
-
+	  (string-match "<b>Due on:</b>\\([^<]+\\)" desc)
+	  (setq deadline (match-string 1 desc))))
+    (string-match
+     "\\(?:Ticket\\|Milestone\\):</b> <a[^>]+>\\([^<]+\\)</a"
+     desc)
+    (setq headline (match-string 1 desc))
+    (string-match "<hr />\\(?:\n\\|<p>\\|\\\\n\\| \\)+\\([^]<]+\\)" desc)
+    (setq description (match-string 1 desc))
+    (concat
+     "* TODO [["
+     link
+     "]["
+     headline
+     "]]"
+     (if (not (null deadline))
+	 (concat
+	  "\nDEADLINE: <"
+	  (format-time-string
+	   "%Y-%m-%d %a"
+	   (apply 'encode-time
+		  (org-fix-decoded-time (parse-time-string deadline))))
+	  ">")
+       "")
+     "\n"
+     description)))
   
 (provide 'org-feed-active-collab)
--- tests.el
+++ tests.el
@@ -16,14 +16,34 @@
       (ert-should (equal result
 			 "* TODO [[https://www.example.com/ac/index.php?path_info=projects/731/tickets/3828][Research domain registrars and their APIs]]\nDEADLINE: <2011-12-01 Thu>\nresearch domain/SSL alternatives to SRS Plus that meet our API/automation needs. Make a recommendation to Henry Hatford as to service to move to (to be implemented in future milestone). in meantime, can apply SSL process be handed to not-Jimmy Dalton?")))))
 
+(ert-deftest ac-formatter-matches-regardless-of-whitespace ()
+  (let ((entry
+	 (list
+	  :guid "https://www.example.com/ac/index.php?path_info=projects/123"
+	  :description "<![CDATA[<p>\n  <b>Project:</b> <a href=\"https://www.example.com/ac/index.php?path_info=projects/123\" class=\"project_link\">FOO: 0013 - Project Common Code Dev</a><br />\n  <b>Milestone:</b> <a href=\"https://www.example.com/ac/index.php?path_info=projects/123/milestones/12345\">Project Future</a><br />\n  <b>Priority:</b> Normal<br />\n  <b>Due on:</b> Dec 30. 2013<br />\n  <b>Assignees:</b> <a href=\"https://www.example.com/ac/index.php?path_info=people/345#user18\">Bill Brasky</a> is responsible. Other assignees: <a href=\"https://www.example.com/ac/index.php?path_info=people/345#user19\">Bradly Manning</a>, <a href=\"https://www.example.com/ac/index.php?path_info=people/345#user36\">Tester McTestersob</a>, <a href=\"https://www.example.com/ac/index.php?path_info=people/345#user51\">Bonnie McPlough</a>.\n</p>\n\n<hr />\n<p>Unscheduled features, enhancements, and bug fixes.</p>]]>")))
+    (let ((result (org-feed-parse-activecollab-entry entry)))
+      (ert-should (equal
+		   result
+		   "* TODO [[https://www.example.com/ac/index.php?path_info=projects/123][Project Future]]\nDEADLINE: <2013-12-30 Mon>\nUnscheduled features, enhancements, and bug fixes.")))))
+		 
+
 (ert-deftest ac-formatter-test-entry-with-no-due-date ()
   (let ((entry
 	 (list
 	  :guid "https://www.example.com/ac/index.php?path_info=projects/435/tickets/1234"
-	  :description "<![CDATA[
-<p> <b>Project:</b> <a href=\"https://www.example.com/ac/index.php?path_info=projects/435\" class=\"project_link\">FOO: 0013 - Project Common </a><br /> <b>Ticket:</b> <a href=\"https://www.example.com/ac/index.php?path_info=projects/435/tickets/1234\">Implement Frabjobber Bozzler rules</a><br /> <b>Priority:</b> Low<br /> <b>Assignees:</b> <a href=\"https://www.example.com/ac/index.php?path_info=people/354#user36\">Tester McTesterson</a> is responsible. Other assignees: <a href=\"https://www.example.com/ac/index.php?path_info=people/354#user18\">Bill Brasky</a>. </p> <hr /> Should rules live in the bumbler directory?]]>")))
+	  :description "<![CDATA[<p>
+  <b>Project:</b> <a href=\"https://www.example.com/ac/index.php?path_info=projects/435\" class=\"project_link\">FOO: 0013 - Project Common Code Dev</a><br />
+  <b>Ticket:</b> <a href=\"https://www.example.com/ac/index.php?path_info=projects/435/tickets/1234\">Implement Frabjobber Bozzler rules</a><br />
+  <b>Priority:</b> Low<br />
+  <b>Assignees:</b> <a href=\"https://www.example.com/ac/index.php?path_info=people/345#user36\">Tester McTestersob</a> is responsible. Other assignees: <a href=\"https://www.example.com/ac/index.php?path_info=people/345#user18\">Bill Brasky</a>.
+</p>
+
+<hr />
+Should rules live in the bumbler directory?]]>")))
     (let ((result (org-feed-parse-activecollab-entry entry)))
       (ert-should (equal
 		   result
-		   "* TODO [[https://www.example.com/ac/index.php?path_info=projects/435/tickets/1234][Implement Frabjobber Bozzler rules]]\n Should rules live in the bumbler directory?")))))
+		   "* TODO [[https://www.example.com/ac/index.php?path_info=projects/435/tickets/1234][Implement Frabjobber Bozzler rules]]\nShould rules live in the bumbler directory?")))))
+
+
 
